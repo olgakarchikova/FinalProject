df5a70e1568bf09bf52066119c058d6c086d7519
Karchikova_Olga <olgasolo1@yandex.ru>
2022-12-08 17:15:14 +0300

    Task added

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] myArray = {"Hello", "--", "5", "Bye", "Olga"};
+string [] newArray = new string[myArray.Length];
+
+void Replace(string [] myarray, string [] newArray)
+{
+    int count = 0;
+    for(int i=0; i < myArray.Length; i++)
+        if (myArray[i].Length <= 3)
+        {
+            newArray[count] = myArray[i];
+            count ++;
+        }
+}
+
+void ShowArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i ++)
+        Console.Write(array[i] + " ");
+}
+
+Replace(myArray, newArray);
+ShowArray(newArray);
